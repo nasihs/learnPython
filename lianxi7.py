a003478f919b486737dcebe068d973df2ceb9dca
nasihs <nasihs1113@gmail.com>
2020-01-21 16:11:04 +0800

    字符串

--- lianxi7.py
+++ lianxi7.py
@@ -1,3 +1,16 @@
-s1 = r'\'hello, world!\''
-s2 = r'\n\\hello, world!\\\n'
-print(s1, s2, end='')
+"""
+字符串
+"""
+
+str1 = 'hello, world!'
+# 通过内置函数len计算字符串的长度
+print(len(str1))  # 13
+# 获得字符串首字母大写的拷贝
+print(str1.capitalize())  # Hello, world!
+# 获得字符串每个单词首字母大写的拷贝
+print(str1.title())  # Hello, World!
+# 获得字符串变大写后的拷贝
+print(str1.upper())  # HELLO, WORLD!
+# 从字符串中查找子串所在位置
+print(str1.find('or'))  # 8
+print(str1.find('shit'))  # -1
